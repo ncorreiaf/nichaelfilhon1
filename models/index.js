0be2862098bea576cafd2f4fc1d37dc84096f332
Luiz Eduardo <124412405+LuizPimenteldev@users.noreply.github.com>
2025-06-14 23:23:17 -0300

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+});
+
+const Livro = sequelize.define('Livro', {
+  titulo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  autor: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  ano: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+
+module.exports = { sequelize, Livro };
